ed0335a79dc9f987a78d135552f8b9ca9ed63670
ShuGang Yao <shugang@protonmail.com>
2022-03-25 17:00:33 +0800

    use  GitHub Actions

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -29,6 +29,8 @@ export default defineUserConfig<DefaultThemeOptions>({
       },
       '/zh/': {
         selectLanguageName: '简体中文',
+        selectLanguageText: '选择语言',
+        selectLanguageAriaLabel: '选择语言',
       },
     },
   },
