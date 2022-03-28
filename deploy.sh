1381dd9753d3d19649e5eeb38145e24fde838351
ShuGang Yao <shugang@protonmail.com>
2022-03-28 18:34:13 +0800

    update www.si

--- deploy.sh
+++ deploy.sh
@@ -10,11 +10,11 @@ npx npm run build
 cd .vuepress/dist
 
 # 如果是发布到自定义域名
-echo 'wiki.syber.ink' > CNAME
+echo 'www.syber.ink' > CNAME
 
 git init
 git add -A
-git commit -m 'deploy wiki.syber.ink' 
+git commit -m 'deploy www.syber.ink' 
 
 # 发布到 gh-pages
 git push -f git@github.com:syberink/SyberInkWiki.git master:gh-pages
