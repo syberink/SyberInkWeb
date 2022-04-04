50cf803934bc901b2cea26dd3ac5bdd38f889c5e
shugangyao <shugang@protonmail.com>
2022-04-04 18:25:34 +0800

    deploy www.syber.ink : V0.21:update

---
+++ assets/welcome.html.5eb5e1cb.js
@@ -0,0 +1,15 @@
+const e={key:"v-1abeb830",path:"/welcome.html",title:"Welcome to Syber.Ink",lang:"en-US",frontmatter:{index:1,title:"Welcome to Syber.Ink",author:"Syber.Ink",date:"2022-03-28T00:00:00.000Z",category:["Guide"],tag:["Welcome"],sticky:!0,star:!0,summary:`Syber Ink : Simplify ink/sign-ing of Syber Era, including multi-signature, unified-login, agreement-inking and signature-verification on Multi Chain with privacy protection based on Zero-Knowledge-Proof technology.
+You can support us at:
+
+gitcoin.co :
+dorahacks.io :
+devpost.com :
+
+`,head:[["meta",{property:"og:url",content:"https://syber.ink/welcome.html"}],["meta",{property:"og:title",content:"Welcome to Syber.Ink"}],["meta",{property:"og:type",content:"article"}],["meta",{property:"og:locale",content:"en-US"}],["meta",{property:"og:locale:alternate",content:"zh-CN"}],["meta",{property:"article:author",content:"Syber.Ink"}],["meta",{property:"article:tag",content:"Welcome"}],["meta",{property:"article:published_time",content:"2022-03-28T00:00:00.000Z"}]]},excerpt:`<p>Syber Ink : Simplify ink/sign-ing of Syber Era, including multi-signature, unified-login, agreement-inking and signature-verification on Multi Chain with privacy protection based on Zero-Knowledge-Proof technology.</p>
+<p>You can support us at:</p>
+<ol>
+<li>gitcoin.co :</li>
+<li>dorahacks.io :</li>
+<li>devpost.com :</li>
+</ol>
+`,headers:[{level:2,title:"Syber Ink",slug:"syber-ink",children:[]},{level:2,title:"Syber Connect",slug:"syber-connect",children:[]},{level:2,title:"Why SyberInk?",slug:"why-syberink",children:[]},{level:2,title:"Tech Stack to Build Syber Ink",slug:"tech-stack-to-build-syber-ink",children:[{level:3,title:"Identity Management",slug:"identity-management",children:[]},{level:3,title:"Account System",slug:"account-system",children:[]},{level:3,title:"Decentralized Networks",slug:"decentralized-networks",children:[]}]}],git:{createdTime:1648461768e3,updatedTime:1649067012e3,contributors:[{name:"ShuGang Yao",email:"shugang@protonmail.com",commits:2},{name:"shugangyao",email:"shugang@protonmail.com",commits:1}]},readingTime:{minutes:.53,words:159},filePathRelative:"welcome.md"};export{e as data};
